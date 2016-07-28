4558123ab02570e6bd64e06a56096ede44a3077a
Kevin Baxter <kbax6342@gmail.com>
2016-07-28 17:25:50 -0400

    second code challenge

---
+++ script.js
@@ -0,0 +1,21 @@
+
+    
+//Code for footer icons    
+(function(){
+window.switchTo5x=false;
+    
+var e=document.createElement("script");
+    
+e.type="text/javascript";
+    
+e.async=true;
+    
+e.onload=function(){try{stLight.options({publisher: "0cf01364-1029-4fde-bce9-d5f6d2419649-a51c", doNotHash: false, doNotCopy: false, hashAddressBar: false});}catch(e){}};
+    
+e.src=("https:" == document.location.protocol ? "https://ws" : "http://w") + ".sharethis.com/button/buttons.js";
+    
+var s = document.getElementsByTagName("script")[0];
+    
+s.parentNode.insertBefore(e, s);})();
+
+
---
+++ scripts.js
@@ -0,0 +1,14 @@
+var button = $("#enter-button");
+
+
+//When you hit the submit button it changes color
+$(button).click(function(){
+    $(this).css("color","white");
+    $(this).css("background-color", "blue");
+});
+
+
+
+
+   
+
